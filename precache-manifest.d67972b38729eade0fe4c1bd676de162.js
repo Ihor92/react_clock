d2a02856b087220fa95cdd3ab9cdeb4cd2b4ef71
Ihor Salfetnikov <salfetnikov92@gmail.com>
2021-02-05 13:16:20 +0200

    Updates

---
+++ precache-manifest.d67972b38729eade0fe4c1bd676de162.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d89e3b11f15182ccc44f172be1483f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "74f15dee6e2d3250bd87",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "33e27156f4d3fa24d47b",
+    "url": "./static/js/2.021d3507.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.021d3507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f15dee6e2d3250bd87",
+    "url": "./static/js/main.b2fb2a52.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
